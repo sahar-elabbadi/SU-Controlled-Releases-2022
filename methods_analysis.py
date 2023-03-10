4c506ef0ba79bfed9df80c144028af255b8a6e9e
sea <sahar.elabbadi@gmail.com>
2023-03-09 17:19:54 -0800

    Save overpass summary data

--- methods_analysis.py
+++ methods_analysis.py
@@ -14,7 +14,7 @@ from plot_methods import abbreviate_op_name
 
 
 # %% method summarize_qc
-def evaluate_overpass_qc(operator, operator_report, operator_meter):
+def evaluate_overpass_qc(operator, stage, operator_report, operator_meter):
     """Generate clean dataframe for each overpass with columns indicating QC status.
 
     Columns are:
@@ -25,6 +25,7 @@ def evaluate_overpass_qc(operator, operator_report, operator_meter):
       - stanford_kept
       - pass_all_qc
     """
+    op_ab = abbreviate_op_name(operator)
 
     # Combine operator report and meter data
     combined_df = operator_report.merge(operator_meter, on='PerformerExperimentID')
@@ -68,6 +69,7 @@ def evaluate_overpass_qc(operator, operator_report, operator_meter):
     ]
 
     operator_qc['qc_summary'] = np.select(qc_conditions, qc_choices, 'ERROR')
+    operator_qc.to_csv(pathlib.PurePath('03_results', 'overpass_summary', f'{op_ab}_{stage}_overpasses.csv'))
     return operator_qc
 
 
@@ -106,7 +108,7 @@ def summarize_qc(operator, stage, operator_report, operator_meter):
     op_ab = abbreviate_op_name(operator)
 
     # Generate dataframe with all relevant QC information
-    operator_qc = evaluate_overpass_qc(operator, operator_report, operator_meter)
+    operator_qc = evaluate_overpass_qc(operator, stage, operator_report, operator_meter)
 
     # Determine how many were QC'ed by Stanford
     total_overpasses = len(operator_qc)
@@ -133,56 +135,3 @@ def summarize_qc(operator, stage, operator_report, operator_meter):
 
     qc_summary.to_csv(pathlib.PurePath('03_results', 'qc_comparison', f'{op_ab}_{stage}_qc.csv'))
     return qc_summary
-
-#
-# def evaluate_overpass_qc(operator, operator_report, operator_meter):
-#     """Generate clean dataframe for each overpass with columns indicating QC status.
-#
-#     Columns are:
-#       - overpass_id
-#       - zero_release
-#       - non_zero_release
-#       - operator_kept
-#       - stanford_kept
-#       - pass_all_qc
-#     """
-#
-#     # Combine operator report and meter data
-#     combined_df = operator_report.merge(operator_meter, on='PerformerExperimentID')
-#
-#     op_ab = abbreviate_op_name(operator)
-#
-#     # Make column with easier name for coding for now.
-#     combined_df['release_rate_kgh'] = combined_df['Last 60s (kg/h) - from Stanford']
-#
-#     # Philippine reports if we discard or not (discard = 1, keep = 0). Change this to have 1 if we keep, 0 if we discard
-#     combined_df['stanford_kept'] = (1 - combined_df['QC: discard - from Stanford'])
-#
-#     # Make dataframe with all relevant info
-#     operator_qc = pd.DataFrame()
-#     operator_qc['overpass_id'] = combined_df.PerformerExperimentID
-#     operator_qc['zero_release'] = combined_df.release_rate_kgh == 0
-#     operator_qc['non_zero_release'] = combined_df.release_rate_kgh != 0  # True if we conducted a release
-#     operator_qc['operator_kept'] = combined_df.OperatorKeep
-#     operator_qc['stanford_kept'] = combined_df.stanford_kept == 1
-#     operator_qc['pass_all_qc'] = operator_qc.stanford_kept * operator_qc.operator_kept
-#
-#     # Make a QC summary column
-#
-#     # Pass all QC
-#     if operator_qc['pass_all_qc'] == 1:
-#         qc_summary = 'pass_all'
-#
-#     # Pass operator
-#     if operator_qc['operator_kept'] == 1:
-#         if operator_qc['stanford_kept'] == 0:
-#             qc_summary = 'fail_stanford'
-#         else:
-#             qc_summary = 'fail_both'
-#
-#     # Pass operator
-#     elif operator_qc['operator_kept'] == 0:
-#         if operator_qc['stanford_kept'] == 1:
-#             qc_summary = 'fail_operator'
-#
-#     operator_qc['qc_summary'] = qc_summary
